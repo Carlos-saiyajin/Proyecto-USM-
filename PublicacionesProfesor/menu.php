2efaaf753977280e4829fb98f9fc688310997683
Carlos Da Silva <carlosdasilva2905@gmail.com>
2025-01-06 16:27:17 -0400

    Se agregó la bandeja de asistencias

--- PublicacionesProfesor/menu.php
+++ PublicacionesProfesor/menu.php
@@ -43,8 +43,9 @@
     <div class="menu">
         <a href="menu_index.php"><button>Publicaciones</button></a>
         <a href="calendario.php"><button>Calendario</button></a>
-        <a href="close.php"><button>Cerrar Sesión</button></a>
         <a href="comentar_sql.php"><button>Bandeja De Comentarios</button></a>
+        <a href="bandeja_asistencia.php"><button>Bandeja de Asistencias</button></a>
+        <a href="close.php"><button>Cerrar Sesión</button></a>
     </div>
 </body>
 </html>
